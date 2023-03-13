4407ec8ab80bd60f15a47704d5d48044685dfb74
STR23X <126894199+STR23X@users.noreply.github.com>
2023-03-13 20:57:06 +0100

    Update script_conf.sh

--- script_conf.sh
+++ script_conf.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 install_Appach () {
 sudo apt-get install apache2
 sudo systemctl restart apache2
@@ -11,7 +10,7 @@ sudo chmod  -R 755 /var/www/html/2048
 
 upload_code () {
 cd /var/www/html/2048
-sudo  git init
+sudo git init
 read -p "Entrer le lien de code source que vous voulez Chargé: "  URL
 sudo  git pull $URL
 }
@@ -19,10 +18,10 @@ sudo  git pull $URL
 acc_srv_web () {
   # Obtient l'adresse IP publique de la machine
   ip_address=$(curl -s icanhazip.com)
+
   # Affiche le lien du site web
   echo "Le site web est accessible à l'adresse : http://$ip_address/2048"
 }
-
 install_Appach
 upload_code
 acc_srv_web
